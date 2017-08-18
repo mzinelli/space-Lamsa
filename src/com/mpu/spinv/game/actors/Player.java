9200272badd9caf7fd8c78bbcb0bcb5baf5e5f22
Annatsu <brendonpagano@gmail.com>
2017-08-18 14:54:33 -0300

    [ Brendon ] Player now has a functional animation using the spritesheet

--- src/com/mpu/spinv/game/actors/Player.java
+++ src/com/mpu/spinv/game/actors/Player.java
@@ -1,6 +1,9 @@
 package com.mpu.spinv.game.actors;
 
+import com.mpu.spinv.engine.model.Animation;
 import com.mpu.spinv.engine.model.GameObject;
+import com.mpu.spinv.engine.model.Sprite;
+import com.mpu.spinv.engine.model.SpriteSheet;
 
 /**
  * Player.java
@@ -11,16 +14,28 @@ import com.mpu.spinv.engine.model.GameObject;
 public class Player extends GameObject {
 
 	// ---------------- Constants ----------------
-	
+
 	private static final int INITIAL_X = 0;
 	private static final int INITIAL_Y = 0;
 
 	private static final boolean INITIAL_VISIBILITY = true;
 
 	// -------------------------------------------
+	
+	private SpriteSheet spriteSheet;
 
-	public Player() {
-		super(INITIAL_X, INITIAL_Y, INITIAL_VISIBILITY);
+	public Player(SpriteSheet spriteSheet) {
+		super(INITIAL_X, INITIAL_Y, new Animation(new Sprite[] {
+				new Sprite(spriteSheet.getSprite(0, 0, 32, 32)),
+				new Sprite(spriteSheet.getSprite(32, 0, 32, 32)),
+				new Sprite(spriteSheet.getSprite(64, 0, 32, 32))
+		}, 20, Animation.ALTERNATIVE_LOOP, true), INITIAL_VISIBILITY);
+		this.spriteSheet = spriteSheet;
+	}
+	
+	@Override
+	public void init() {
+		startAnimation();
 	}
 
 }
