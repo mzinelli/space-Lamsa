fdcf9e6895282009e337e5820c7ebf4ccec42415
Brendon Pagano <brendonpagano@gmail.com>
2017-09-30 22:01:19 -0300

    [Brendon] Added a HelpText class and refactored Score class code

--- src/com/mpu/spinv/game/states/GameplayState.java
+++ src/com/mpu/spinv/game/states/GameplayState.java
@@ -3,6 +3,7 @@ package com.mpu.spinv.game.states;
 import com.mpu.spinv.engine.model.State;
 import com.mpu.spinv.game.states.gameplaystate.AlienGroup;
 import com.mpu.spinv.game.states.gameplaystate.Background;
+import com.mpu.spinv.game.states.gameplaystate.HelpText;
 import com.mpu.spinv.game.states.gameplaystate.LifeBar;
 import com.mpu.spinv.game.states.gameplaystate.Player;
 import com.mpu.spinv.game.states.gameplaystate.Score;
@@ -27,6 +28,7 @@ public class GameplayState extends State {
 	
 	private LifeBar lifebar;
 	private Score score;
+	private HelpText helpText;
 	
 	private Player player;
 	private AlienGroup alienGroup;
@@ -43,12 +45,14 @@ public class GameplayState extends State {
 		score = new Score();
 		player = new Player(score);
 		alienGroup = new AlienGroup();
+		helpText = new HelpText();
 
 		addResource("background", background);
 		addResource("player", player);
 		addResource("alien-group", alienGroup);
 		addResource("lifebar", lifebar);
 		addResource("score", score);
+		addResource("help-text", helpText);
 	}
 
 }
--- src/com/mpu/spinv/game/states/gameplaystate/HelpText.java
+++ src/com/mpu/spinv/game/states/gameplaystate/HelpText.java
@@ -6,29 +6,25 @@ import java.awt.Graphics;
 import com.mpu.spinv.engine.model.GameText;
 import com.mpu.spinv.utils.Constants;
 
-public class Score extends GameText {
-	
+public class HelpText extends GameText {
+
 	// ---------------- Constants ----------------
-	
+
 	private static final int X = 0;
-	private static final int Y = 10;
-	
+	private static final int Y = 0;
+
 	private static final boolean INITIAL_VISIBILITY = true;
-	
-	private static final String SCORE_TEXT = "SCORE: 0000";
+
+	private static final String TEXT = "ESC - MENU" + "\n" +
+									   "F1     - HELP";
 	private static final Color TEXT_COLOR = Color.WHITE;
-	
+
 	// -------------------------------------------
 	
-	/**
-	 * The player's score.
-	 */
-	private int score = 0;
-	
 	private boolean _firstTime = true;
-	
-	public Score() {
-		super(X, Y, INITIAL_VISIBILITY, SCORE_TEXT, TEXT_COLOR);
+
+	public HelpText() {
+		super(X, Y, INITIAL_VISIBILITY, TEXT, TEXT_COLOR);
 	}
 	
 	@Override
@@ -37,24 +33,9 @@ public class Score extends GameText {
 		
 		if (_firstTime) {
 			setX(Constants.WINDOW_WIDTH - 4 - width - 10);
+			setY(Constants.WINDOW_HEIGHT - 30 - height - 10);
 			_firstTime = false;
 		}
-	}
-	
-	// Getters and Setters
-	
-	public void increment(int n) {
-		score += n;
-		setText(SCORE_TEXT.substring(0, SCORE_TEXT.length() - Integer.toString(score).length()) + score);
-	}
-	
-	public void decrement(int n) {
-		score -= n;
-		setText(SCORE_TEXT + score);
-	}
-	
-	public int getScore() {
-		return score;
 	}
 
 }
