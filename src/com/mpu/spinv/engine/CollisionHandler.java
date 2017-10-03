502571c8c8bfd3f95e1f42ce48dc3e2b649ba9b5
Brendon Pagano <brendonpagano@gmail.com>
2017-10-02 22:25:44 -0300

    [Brendon] Added a new way to make collision tests

--- src/com/mpu/spinv/engine/CollisionHandler.java
+++ src/com/mpu/spinv/engine/CollisionHandler.java
@@ -11,13 +11,16 @@ import com.mpu.spinv.engine.model.GameObject;
 public class CollisionHandler {
 
 	public static boolean hasCollided(GameObject obj0, GameObject obj1) {
-		// Intersects on the right edge of obj0.
-		if (obj0.getX() < obj1.getX() + obj1.getWidth() &&
-				obj0.getX() + obj0.getWidth() > obj1.getX() &&
-				obj0.getY() + obj0.getHeight() > obj1.getY() &&
-				obj0.getY() < obj1.getY() + obj1.getHeight()) {
+		return CollisionHandler.hasCollided(obj0.getX(), obj0.getY(), obj0.getX() + obj0.getWidth(), obj0.getY() + obj0.getHeight(),
+				obj1.getX(), obj1.getY(), obj1.getX() + obj1.getWidth(), obj1.getY() + obj1.getHeight());
+	}
+	
+	public static boolean hasCollided(int x0, int y0, int xw0, int yh0, int x1, int y1, int xw1, int yh1) {
+		if (x0 < xw1 &&
+				xw0 > x1 &&
+				yh0 > y1 &&
+				y0 < yh1)
 			return true;
-		}
 		
 		return false;
 	}
