6cecc4f833874bd3cc849f9a738724d77afa05db
Miguel-PC\Miguel <miguel.zinelli@msn.com>
2018-01-22 00:54:49 -0200

    [Miguel] - Almost solving boss movimentation problem

--- src/com/mpu/spinv/game/states/gameplaystate/Boss.java
+++ src/com/mpu/spinv/game/states/gameplaystate/Boss.java
@@ -16,35 +16,36 @@ import com.mpu.spinv.utils.Constants;
  * @date 2017-12-15
  */
 public class Boss extends GameEntity {
-	
+
 	// ---------------- Constants ----------------
-	
+
+	// ---------------------------------------------
 	private static final int WIDTH = 183;
 	private static final int HEIGHT = 162;
-	
+
 	private static final int INITIAL_X = Constants.WINDOW_WIDTH / 2 - WIDTH / 2;
 	private static final int INITIAL_Y = 10;
-	
+
 	private final int VELOCITY = 5;
-	
+
 	private static final boolean INITIAL_VISIBILITY = false;
-	
+
 	// Entity features
-	
+
 	private final int SCORE_VALUE = 50;
-	
+
 	private int life = 10;
-	
+
 	/**
 	 * A reference to the score object.
 	 */
 	private Score score;
-	
+
 	public Boss(Score score) {
 		super(INITIAL_X, INITIAL_Y, INITIAL_VISIBILITY);
-		
+
 		this.score = score;
-		
+
 		// Setting up the boss sprite
 		BufferedImage image = null;
 		try {
@@ -53,33 +54,33 @@ public class Boss extends GameEntity {
 			e.printStackTrace();
 		}
 		setStaticSprite(new Sprite(image));
-		
+
 		// Boss setup
 		setScreenBound(true);
 		setVelocity(VELOCITY, VELOCITY);
 		moveOnlyIfVisible(true);
-		setX(500);
+		setX(900);
 		setY(20);
-		setVelocityX(1);
+		setVelocityX(4);
 
-		ir();
 	}
-	
-//		if (getX() >= Constants.WINDOW_WIDTH) {
-//			
-//			moveLeft(true);
-		//}
-	
-	public void ir(){
-		this.moveRight(1);
+
+	@Override
+	public void update() {
+		moveLeft(true);
+		if (x + width > Constants.WINDOW_WIDTH - 4 - 10)
+			moveLeft(true);
+		else if (x < 10)
+			moveRight(true);
+		super.update();
 	}
-	
+
 	@Override
 	public void die() {
 		super.die();
 		score.increment(SCORE_VALUE);
 	}
-	
+
 	public void decrementLife() {
 		if (life > 1)
 			life--;
